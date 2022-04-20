cc3dcc90bb575af9e596815ec453f0245bab9dbf
BlessedEmoefe <blessedemoefe1122334455@gmail.com>
2022-04-20 23:52:46 +0100

    created hideableText component

---
+++ src/HideableText.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class HideableText extends React.Component{
+    constructor(props){
+        super( props)
+    }
+    render(){
+        return(
+            <div>This is the Hideable Text</div>
+        )
+    }
+}
