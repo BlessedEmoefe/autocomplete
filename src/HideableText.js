d7a64c42953d76de9671657d712e70382a2268a5
BlessedEmoefe <blessedemoefe1122334455@gmail.com>
2022-04-20 23:57:38 +0100

    added state to hideableText component

--- src/HideableText.js
+++ src/HideableText.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-export default class HideableText extends React.Component{
-    constructor(props){
-        super( props)
-    }
-    render(){
-        return(
-            <div>{this.props.text}</div>
-        )
-    }
+export default class HideableText extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHidden: false,
+    };
+  }
+  render() {
+    return <div>{this.props.text}</div>;
+  }
 }
